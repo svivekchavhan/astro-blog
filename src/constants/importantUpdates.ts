cc6a0d51c8946cee6125aa3230b1e2e5e8b565e7
Vivek Chavhan <45397449+svivekchavhan@users.noreply.github.com>
2026-08-07 17:14:10 +0530

    feat: add MobileUrgentBoard component and MPSC Group C exam pages

--- src/constants/importantUpdates.ts
+++ src/constants/importantUpdates.ts
@@ -9,6 +9,15 @@ export interface ImportantUpdate {
 
 export const importantUpdates: ImportantUpdate[] = [
   {
+    id: "mpsc-group-c-pesa-update-2026",
+    title:
+      "MPSC गट-क परीक्षा: पेसा (PESA) रहिवासी व माजी सैनिक आरक्षणाबाबत परिपत्रक जारी",
+    link: "/mpsc-group-c-pesa-update-2026",
+    date: "August 07, 2026",
+    expiry: "2026-08-30T00:00:00.000Z",
+    show: true,
+  },
+  {
     id: "mahajyoti-free-coaching-exam-timetable-2026",
     title: "महाज्योति परीक्षा सुधारित वेळापत्रक",
     link: "/mahajyoti-free-coaching-exam-timetable-2026",
