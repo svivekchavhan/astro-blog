0861a30fd04b7bddc468d9cd3e1e80c24f1624e2
Vivek Chavhan <45397449+svivekchavhan@users.noreply.github.com>
2026-08-09 10:39:25 +0530

    feat: add importantUpdates constants file to project directory

--- src/constants/importantUpdates.ts
+++ src/constants/importantUpdates.ts
@@ -9,15 +9,6 @@ export interface ImportantUpdate {
 
 export const importantUpdates: ImportantUpdate[] = [
   {
-    id: "mumbai-police-final-selection-list-2026",
-    title:
-      "🔥 मुंबई पोलीस शिपाई भरती २०२४-२५: ३,५२१ पदांची अंतिम निवड यादी (Final Selection List) व प्रतीक्षा यादी जाहीर",
-    link: "/mumbai-police-constable-final-selection-list-2026",
-    date: "August 07, 2026",
-    expiry: "2026-09-15T00:00:00.000Z",
-    show: true,
-  },
-  {
     id: "mpsc-group-c-pesa-update-2026",
     title:
       "MPSC गट-क परीक्षा: पेसा (PESA) रहिवासी व माजी सैनिक आरक्षणाबाबत परिपत्रक जारी",
@@ -31,24 +22,6 @@ export const importantUpdates: ImportantUpdate[] = [
     title: "महाज्योति परीक्षा सुधारित वेळापत्रक",
     link: "/mahajyoti-free-coaching-exam-timetable-2026",
     date: "August 04, 2026",
-    expiry: "2026-08-30T00:00:00.000Z",
-    show: true,
-  },
-  {
-    id: "mpsc-group-c-last-date-extended-2026",
-    title:
-      "MPSC गट-क संयुक्त पूर्व परीक्षा २०२६: मुदतवाढ शुद्धिपत्रक प्रसिद्ध! १० ऑगस्ट २०२६ पर्यंत अर्ज करण्यास मुदतवाढ",
-    link: "/mpsc-group-c-last-date-extended-2026",
-    date: "August 05, 2026",
-    expiry: "2026-08-30T00:00:00.000Z",
-    show: true,
-  },
-  {
-    id: "latur-dcc-revised-waiting-list",
-    title:
-      "लातूर जिल्हा मध्यवर्ती बँक भरती २०२६: सुधारीत प्रतीक्षा यादी (Revised Waiting List) प्रसिद्ध",
-    link: "/latur-dcc-bank-revised-waiting-list-2026",
-    date: "August 03, 2026",
     expiry: "2026-08-30T00:00:00.000Z",
     show: true,
   },
