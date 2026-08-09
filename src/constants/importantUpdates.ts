1d7a411eb6bc7a669029a2ca2ddbe3ed278612d1
Vivek Chavhan <45397449+svivekchavhan@users.noreply.github.com>
2026-08-09 10:55:01 +0530

    feat: add MobileUrgentBoard component for displaying urgent updates on mobile devices

--- src/constants/importantUpdates.ts
+++ src/constants/importantUpdates.ts
@@ -11,7 +11,7 @@ export const importantUpdates: ImportantUpdate[] = [
   {
     id: "pavitra-portal-tait-2025-preference-notice",
     title:
-      "🔥 पवित्र पोर्टल TAIT-2025: पवित्र पोर्टल Log In Problem बाबतीत सूचना ",
+      "🔥 पवित्र पोर्टल TAIT-2025: पवित्र पोर्टल वरील Log In Problem बाबतीत सूचना ",
     link: "/pavitra-portal-tait-2025-preference-filling-notice",
     date: "August 08, 2026",
     expiry: "2026-09-30T00:00:00.000Z",
